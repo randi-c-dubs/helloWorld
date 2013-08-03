bdc8dfafa794c53f39120ae153ffa3579ada3298
Juan Sebastian Angarita <alosoleil@gmail.com>
2013-08-02 23:28:27 -0700

    Testing statics

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask
-app = Flask(__name__, static_url_path='/', static_folder='html')
+app = Flask(__name__, static_url_path='', static_folder='./html/')
+
+@app.route('/')
+def root():
+return app.send_static_file('index.html')
 
 @app.route("/api")
 def html():
     return "Works!"
-
 
 
 
