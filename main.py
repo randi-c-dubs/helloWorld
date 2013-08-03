3e8a718b2368917979d1f150d5e5f2c590ca250e
Juan Sebastian Angarita <alosoleil@gmail.com>
2013-08-02 23:20:14 -0700

    Testing statics

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/api")
+def html():
+    return "Works!"
+
+
+
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=80, static_url_path='/', static_folder='html')
